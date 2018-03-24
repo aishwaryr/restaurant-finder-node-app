ac9074639390eff786999e86506163f4dc40cb3b
aishwaryr <araj713@gmail.com>
2018-03-25 00:07:13 +0530

    Add search UI

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,16 +1,14 @@
 function autocomplete(input, latInput, lngInput) {
-  if (!input) return; //skip if no input on page
+  if(!input) return; // skip this fn from running if there is not input on the page
   const dropdown = new google.maps.places.Autocomplete(input);
 
-  dropdown.addListener("place_changed", () => {
+  dropdown.addListener('place_changed', () => {
     const place = dropdown.getPlace();
-    // console.log(place);
     latInput.value = place.geometry.location.lat();
     lngInput.value = place.geometry.location.lng();
   });
-
-  // if someone hits enter on address field, don;t submit the form
-  input.on("keydown", e => {
+  // if someone hits enter on the address field, don't submit the form
+  input.on('keydown', (e) => {
     if (e.keyCode === 13) e.preventDefault();
   });
 }
