f3614f768785d8987dc5ea03184b452207a71384
aishwaryr <araj713@gmail.com>
2018-04-01 18:39:56 +0530

    Add pagination

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -5,7 +5,6 @@ const multer = require("multer");
 const jimp = require("jimp");
 const uuid = require("uuid");
 
-// Multer only reads the img and puts it in the temp memory - for resizing and actually storing it we'll use jimp
 const multerOptions = {
   storage: multer.memoryStorage(),
   fileFilter(req, file, next) {
@@ -18,14 +17,12 @@ const multerOptions = {
   },
 };
 
-exports.homepage = (req, res) => {
+exports.homePage = (req, res) => {
   res.render("index");
 };
 
 exports.addStore = (req, res) => {
-  res.render("editStore", {
-    title: "Add Store",
-  });
+  res.render("editStore", { title: "Add Store" });
 };
 
 exports.upload = multer(multerOptions).single("photo");
@@ -33,7 +30,7 @@ exports.upload = multer(multerOptions).single("photo");
 exports.resize = async (req, res, next) => {
   // check if there is no new file to resize
   if (!req.file) {
-    next(); // skip to nxt middleware
+    next(); // skip to the next middleware
     return;
   }
   const extension = req.file.mimetype.split("/")[1];
@@ -42,27 +39,36 @@ exports.resize = async (req, res, next) => {
   const photo = await jimp.read(req.file.buffer);
   await photo.resize(800, jimp.AUTO);
   await photo.write(`./public/uploads/${req.body.photo}`);
-  // once we have written the phot to our filesystem, keep going
+  // once we have written the photo to our filesystem, keep going!
   next();
 };
 
 exports.createStore = async (req, res) => {
   req.body.author = req.user._id;
   const store = await new Store(req.body).save();
-  // await store.save();
-  // console.log("It saved");
   req.flash("success", `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = page * limit - limit;
+
   // 1. Query the database for a list of all stores
-  const stores = await Store.find();
-  // console.log(Store);
-  res.render("stores", {
-    title: "Stores",
-    stores,
-  });
+  const storesPromise = Store.find().skip(skip).limit(limit).sort({ created: "desc" });
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash("info", `Hey! You asked for page ${page}. But that doesn't exist. So I put you on page ${pages}`);
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+
+  res.render("stores", { title: "Stores", stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
@@ -74,45 +80,43 @@ const confirmOwner = (store, user) => {
 exports.editStore = async (req, res) => {
   // 1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id });
-  // res.json(store);
   // 2. confirm they are the owner of the store
   confirmOwner(store, req.user);
-  // 3. Render out the edit form so user can update their store
+  // 3. Render out the edit form so the user can update their store
   res.render("editStore", { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
   // set the location data to be a point
-  // W/o this if we update loc it won't have type - Point
   req.body.location.type = "Point";
   // find and update the store
-  // findOneAndUpdate takes in query, data & some options
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
-    new: true, // return the new updated store instead of old one
-    runValidators: true, // forces the validations defined in Schema to run again on new data (by default they run only once in the begining)
+    new: true, // return the new store instead of the old one
+    runValidators: true,
   }).exec();
-  // Redirect them to the store and tell them it worked
   req.flash(
     "success",
-    `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store >-</a>`
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`
   );
   res.redirect(`/stores/${store._id}/edit`);
+  // Redriect them the store and tell them it worked
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
   const store = await Store.findOne({ slug: req.params.slug }).populate("author reviews");
-  // if store not found then pass to next middleware which is NotFound
   if (!store) return next();
   res.render("store", { store, title: store.name });
 };
 
 exports.getStoresByTag = async (req, res) => {
   const tag = req.params.tag;
-  const tagQuery = tag || { $exists: true };
+  const tagQuery = tag || { $exists: true, $ne: [] };
+
   const tagsPromise = Store.getTagsList();
   const storesPromise = Store.find({ tags: tagQuery });
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
-  res.render("tags", { tags, title: "Tags", tag, stores });
+
+  res.render("tag", { tags, title: "Tags", tag, stores });
 };
 
 exports.searchStores = async (req, res) => {
@@ -128,7 +132,7 @@ exports.searchStores = async (req, res) => {
         score: { $meta: "textScore" },
       }
     )
-    // then sort them
+    // the sort them
     .sort({
       score: { $meta: "textScore" },
     })
@@ -151,7 +155,7 @@ exports.mapStores = async (req, res) => {
     },
   };
 
-  const stores = await Store.find(q).select("slug name description location photo").limit(10); // selecting the fields that we want in JSON response
+  const stores = await Store.find(q).select("slug name description location photo").limit(10);
   res.json(stores);
 };
 
@@ -161,6 +165,7 @@ exports.mapPage = (req, res) => {
 
 exports.heartStore = async (req, res) => {
   const hearts = req.user.hearts.map(obj => obj.toString());
+
   const operator = hearts.includes(req.params.id) ? "$pull" : "$addToSet";
   const user = await User.findByIdAndUpdate(req.user._id, { [operator]: { hearts: req.params.id } }, { new: true });
   res.json(user);
@@ -175,5 +180,5 @@ exports.getHearts = async (req, res) => {
 
 exports.getTopStores = async (req, res) => {
   const stores = await Store.getTopStores();
-  res.render("topStores", { title: "Top Stores", stores });
+  res.render("topStores", { stores, title: "⭐ Top Stores!" });
 };
